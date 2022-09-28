c4e41e44865d98771cb318ed120fd4c2069510a1
Yusuf Çırak <yusuf10901@hotmail.com>
2022-09-28 20:25:59 +0300

    Add project files.

---
+++ src/Projections/Dictionary.FavoriteService/Program.cs
@@ -0,0 +1,10 @@
+using Dictionary.FavoriteService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
---
+++ src/Projections/Dictionary.VoteService/Program.cs
@@ -0,0 +1,10 @@
+using Dictionary.VoteService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
