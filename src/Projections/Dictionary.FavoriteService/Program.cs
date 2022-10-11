1ea8c38c498d799d1e2f2bcb8c0644b2af8b9525
Yusuf Çırak <yusuf10901@hotmail.com>
2022-10-11 19:04:28 +0300

    Services added for worker services

--- src/Projections/Dictionary.FavoriteService/Program.cs
+++ src/Projections/Dictionary.FavoriteService/Program.cs
@@ -1,9 +1,12 @@
 using Dictionary.FavoriteService;
+using Dictionary.FavoriteService.Services;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
         services.AddHostedService<Worker>();
+
+        services.AddTransient<FavoriteService>();
     })
     .Build();
 
--- src/Projections/Dictionary.VoteService/Program.cs
+++ src/Projections/Dictionary.VoteService/Program.cs
@@ -1,9 +1,12 @@
 using Dictionary.VoteService;
+using Dictionary.VoteService.Services;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
         services.AddHostedService<Worker>();
+
+        services.AddTransient<VoteService>();
     })
     .Build();
 
